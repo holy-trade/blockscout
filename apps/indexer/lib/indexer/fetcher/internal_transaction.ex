bf873b5b4da54587803f6860453e342a6dad87a3
Valentin Rodygin <valentin@clabs.co>
2021-09-02 12:04:00 +0200

    Internal tx batch 2, concurrency 20

--- apps/indexer/lib/indexer/fetcher/internal_transaction.ex
+++ apps/indexer/lib/indexer/fetcher/internal_transaction.ex
@@ -23,7 +23,7 @@ defmodule Indexer.Fetcher.InternalTransaction do
   @behaviour BufferedTask
 
   @max_batch_size 2
-  @max_concurrency 50
+  @max_concurrency 20
   @defaults [
     flush_interval: :timer.seconds(3),
     poll_interval: :timer.seconds(3),
