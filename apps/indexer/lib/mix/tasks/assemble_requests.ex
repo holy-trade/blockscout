477b883ee8ee2af97c48caa297d6011820c513b2
Donald Hutchison <donald.hutchison@clabs.co>
2021-09-07 16:16:15 +0200

    Save transaction json.

--- apps/indexer/lib/mix/tasks/assemble_requests.ex
+++ apps/indexer/lib/mix/tasks/assemble_requests.ex
@@ -6,32 +6,52 @@ defmodule Mix.Tasks.AssembleRequests do
   def run(args) do
     HTTPoison.start()
 
-    blocks = args
+    {options, blocks, _} = OptionParser.parse(args, strict: [output: :string])
+    blocks_request = blocks
              |> Enum.map(&(String.split(&1, ~r/[\s,]/)))
              |> List.flatten
              |> Enum.map(&String.to_integer/1)
-             |> get_blocks_json
+             |> build_blocks_request()
 
-    url = System.get_env("ETHEREUM_JSONRPC_HTTP_URL", "")
+    transaction_json =
+      blocks_request
+      |> send_request()
+      |> extract_transactions()
+      |> Enum.map(&build_transactions_request/1)
 
+    if options[:output] do
+      File.write(options[:output], transaction_json)
+    end
 
-    #get list of blocks
-    #map to transactions
-    #build json
-
-    IO.inspect(blocks)
-    send_request(url, blocks)
-    |> IO.inspect()
+    #send_internal_transaction_request(transaction_json)
   end
 
-  def send_request(url, json) do
+  def send_request(json) do
+    url = System.get_env("ETHEREUM_JSONRPC_HTTP_URL", "")
+    start_time = :os.system_time(:millisecond)
     case HTTPoison.post(url, json, [{"Content-Type", "application/json"}]) do
       {:ok, %HTTPoison.Response{body: body, status_code: status_code}} ->
         {:ok, %{body: body, status_code: status_code}}
+      bad_response ->
+        {:fail, bad_response, :os.system_time(:millisecond) - start_time}
     end
   end
 
-  def get_blocks_json(blocks) do
+  def extract_transactions({:ok, %{status_code: 200, body: transaction_body}}) do
+    transaction_body
+    |> Jason.decode!
+    |> Enum.map(fn block_response ->
+      result = block_response["result"]
+      Enum.reduce(result["transactions"], [], fn t,acc ->
+        [ %{block_number: hex_to_int(result["number"]), block_hash: result["hash"], index: t["transactionIndex"], transaction_hash: t["hash"]} | acc ]
+      end)
+      |> Enum.reverse
+    end)
+  end
+
+  def hex_to_int("0x" <> str), do: String.to_integer(str, 16)
+
+  def build_blocks_request(blocks) do
     blocks
     |> Enum.reduce({0,[]} , fn number, {id, requests} ->
       r = %{jsonrpc: "2.0", method: "eth_getBlockByNumber", params: ["0x"<> Integer.to_string(number, 16), true], id: id}
@@ -39,5 +59,19 @@ defmodule Mix.Tasks.AssembleRequests do
     end)
     |> elem(1)
     |> Jason.encode!
+  end
+
+  @tracer_path "../../apps/ethereum_jsonrpc/priv/js/ethereum_jsonrpc/geth/debug_traceTransaction/tracer.js"
+  @external_resource @tracer_path
+  @tracer File.read!(@tracer_path)
+  def build_transactions_request(transactions) do
+    transactions
+    |> Enum.reduce({0,[]} , fn %{transaction_hash: t}, {id, requests} ->
+      r = %{jsonrpc: "2.0", method: "debug_traceTransaction", params: [t, %{tracer: @tracer, timeout: "100s"}], id: id}
+      {id + 1, [r | requests]}
+    end)
+    |> elem(1)
+    |> Jason.encode!
+
   end
 end
