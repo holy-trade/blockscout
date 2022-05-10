af7b44ab9b85df53e2db13a631c260dbc0d62ac8
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-05-10 13:32:08 +0200

    Remove not needed DELETE query

--- apps/explorer/priv/repo/migrations/20211013190346_remove_duplicates_of_current_token_balances.exs
+++ apps/explorer/priv/repo/migrations/20211013190346_remove_duplicates_of_current_token_balances.exs
@@ -3,26 +3,6 @@ defmodule Explorer.Repo.Migrations.RemoveDuplicatesOfCurrentTokenBalances do
 
   def change do
     execute("""
-    DELETE FROM address_current_token_balances
-    WHERE id in (
-        SELECT a.id FROM (SELECT actb.*
-        FROM address_current_token_balances actb
-        INNER JOIN tokens t
-        ON actb.token_contract_address_hash = t.contract_address_hash
-        WHERE t.type='ERC-721') AS a
-    LEFT JOIN
-        (SELECT actb.address_hash, actb.token_contract_address_hash, MAX(actb.value_fetched_at) AS max_value_fetched_at
-        FROM address_current_token_balances actb
-        INNER JOIN tokens t
-        ON actb.token_contract_address_hash = t.contract_address_hash
-        WHERE t.type='ERC-721'
-        GROUP BY token_contract_address_hash, address_hash) c
-    ON a.address_hash=c.address_hash AND a.token_contract_address_hash = c.token_contract_address_hash AND a.value_fetched_at = c.max_value_fetched_at
-    WHERE c.address_hash IS NULL
-    );
-    """)
-
-    execute("""
     UPDATE address_current_token_balances
     SET token_id = NULL
     WHERE id in (
