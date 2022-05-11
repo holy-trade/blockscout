40446583b2fda8e9e2c88537e8ec3956eec3e9c3
enrique <enrique@keyko.io>
2022-05-11 10:54:22 +0200

    Get rid off blocks sanirizer

--- apps/indexer/lib/indexer/supervisor.ex
+++ apps/indexer/lib/indexer/supervisor.ex
@@ -10,7 +10,6 @@ defmodule Indexer.Supervisor do
   alias Indexer.{
     Block,
     CalcLpTokensTotalLiqudity,
-    EmptyBlocksSanitizer,
     PendingOpsCleaner,
     PendingTransactionsSanitizer,
     SetAmbBridgedMetadataForTokens,
@@ -138,7 +137,6 @@ defmodule Indexer.Supervisor do
 
       # Out-of-band fetchers
       {CoinBalanceOnDemand.Supervisor, [json_rpc_named_arguments]},
-      {EmptyBlocksSanitizer, [[json_rpc_named_arguments: json_rpc_named_arguments]]},
       {TokenTotalSupplyOnDemand.Supervisor, [json_rpc_named_arguments]},
       {PendingTransactionsSanitizer, [[json_rpc_named_arguments: json_rpc_named_arguments]]},
 
