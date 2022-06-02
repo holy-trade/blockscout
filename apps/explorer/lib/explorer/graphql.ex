ca2272c25531aaf31cad533ce0010abb6f4651f5
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-06-02 13:25:40 +0200

    Add token_id and token_type to graphql token_tx_transfers_query

--- apps/explorer/lib/explorer/graphql.ex
+++ apps/explorer/lib/explorer/graphql.ex
@@ -372,7 +372,9 @@ defmodule Explorer.GraphQL do
         token: token.symbol,
         token_address: token.contract_address_hash,
         nonce: tx.nonce,
-        block_number: tt.block_number
+        block_number: tt.block_number,
+        token_id: tt.token_id,
+        token_type: token.type
       },
       order_by: [desc: tt.block_number, desc: tt.amount, desc: tt.log_index]
     )
