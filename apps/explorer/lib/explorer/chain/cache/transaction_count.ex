537c53b78252548897cea8d447b09ccb1b4dc7f4
Donald Hutchison <donald.hutchison@clabs.co>
2022-06-14 17:39:25 +0200

    Check db for running transaction count query before running.

--- apps/explorer/lib/explorer/chain/cache/transaction_count.ex
+++ apps/explorer/lib/explorer/chain/cache/transaction_count.ex
@@ -32,16 +32,21 @@ defmodule Explorer.Chain.Cache.TransactionCount do
     {:ok, task} =
       Task.start(fn ->
         try do
-          result = Repo.aggregate(Transaction, :count, :hash, timeout: :infinity)
+          with {:ok, :nothing_running} <- query_db_for_running_tx_count_pids(),
+              result <- query_db_for_exact_count() do
 
-          params = %{
-            counter_type: "total_transaction_count",
-            value: result
-          }
+            params = %{
+              counter_type: "total_transaction_count",
+              value: result
+            }
 
-          Chain.upsert_last_fetched_counter(params)
+            Chain.upsert_last_fetched_counter(params)
 
-          set_count(result)
+            set_count(result)
+          else
+            {:updating, pods} ->
+              Logger.info("Transaction count query is already running on pods #{Enum.join(pods, ",")}")
+          end
         rescue
           e ->
             Logger.debug([
@@ -69,5 +74,26 @@ defmodule Explorer.Chain.Cache.TransactionCount do
       {integer, ""} -> :timer.seconds(integer)
       _ -> @default_cache_period
     end
+  end
+
+
+  # getting count on a large table is very expensive and so we check for a process running on the db level
+  # if so, we will refuse to launch another task and simply take the current estimated value until the running process has updated
+  # the db
+  defp query_db_for_running_tx_count_pids() do
+    %{rows: results} = Ecto.Adapters.SQL.query!(Explorer.Repo, "SELECT application_name, pid
+            FROM pg_stat_activity
+            WHERE lower(query) like 'select count%from \"transactions\"%'")
+
+    if length(results) == 0 do
+      {:ok, :nothing_running}
+    else
+      pods = results |> Enum.map(fn [_pid, podname] -> podname end)
+      {:updating, pods}
+    end
+  end
+
+  defp query_db_for_exact_count() do
+    Repo.aggregate(Transaction, :count, :hash, timeout: :infinity)
   end
 end
