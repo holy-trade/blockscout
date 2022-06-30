6e18210693650b6b14cbc2bb6f343a76d35a9074
Leszek Stachowski <leszek.stachowski@clabs.co>
2022-06-30 09:42:03 +0200

    Run the migrations concurrently

--- apps/explorer/priv/repo/migrations/20211018164843_transactions_block_number_index.exs
+++ apps/explorer/priv/repo/migrations/20211018164843_transactions_block_number_index.exs
@@ -1,11 +1,14 @@
 defmodule Explorer.Repo.Migrations.TransactionsBlockNumberIndex do
   use Ecto.Migration
+  @disable_ddl_transaction true
+  @disable_migration_lock true
 
   def change do
     create(
       index(
         :transactions,
-        ~w(block_number)a
+        ~w(block_number)a,
+        concurrently: true
       )
     )
   end
