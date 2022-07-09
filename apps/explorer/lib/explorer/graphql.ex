fa011115ce9e3f8ec754a54e0e066eec80d04721
bhndt <97825684+bhndt@users.noreply.github.com>
2022-07-10 02:31:17 +0800

    Revert "Expose Token ID in graphQL query (#692)"
    This reverts commit 40cb389e3c272d4e0138ff32904dec61df706323.

--- apps/explorer/lib/explorer/graphql.ex
+++ apps/explorer/lib/explorer/graphql.ex
@@ -373,8 +373,7 @@ defmodule Explorer.GraphQL do
         token_address: token.contract_address_hash,
         nonce: tx.nonce,
         block_number: tt.block_number,
-        token_type: token.type,
-        token_id: tt.token_id
+        token_type: token.type
       },
       order_by: [desc: tt.block_number, desc: tt.amount, desc: tt.log_index]
     )
@@ -430,8 +429,7 @@ defmodule Explorer.GraphQL do
         token_address: tt.token_contract_address_hash,
         nonce: tx.nonce,
         block_number: tt.block_number,
-        token_type: token.type,
-        token_id: tt.token_id
+        token_type: token.type
       },
       order_by: [desc: tt.block_number, desc: tt.amount, desc: tt.log_index]
     )
